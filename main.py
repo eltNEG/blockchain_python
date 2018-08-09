a4e4fdfb647495d4324c49df7e0a47d2d6ae4753
eltNEG <yorewole@gmail.com>
2018-08-09 22:07:11 +0100

    print format, timestamp and newline added

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
+"""Blockchain"""
 from blockchain import Blockchain
 
+
 def main():
-  bc = Blockchain([]).new_blockchain()
+    """Test the blockchain"""
+    bc = Blockchain([]).new_blockchain()
+    bc.add_block("Send 1 BTC to Ivan")
+    bc.add_block("Send 2 more BTC to Ivan")
 
-  bc.add_block("Send 1 BTC to Ivan")
-  bc.add_block("Send 2 more BTC to Ivan")
-
-  for block in bc.blocks:
-    print("Prev. hash: %s\n" % (block.PrevBlockHash), type(block.PrevBlockHash))
-    print("Data: %s\n" % (block.Data))
-    print("Hash: %s\n" % (block.Hash))
-    print("Key: %s\n" % (block.Hash.encode()))
-    
+    for block in bc.blocks:
+        print("Prev. hash: %s" % (block.prev_block_hash))
+        print("Data: %s" % (block.data))
+        print("Hash: %s" % (block.hash))
+        print("Timestamp: %s" % (block.timestamp))
+        print("\n")
 
 
 main()
