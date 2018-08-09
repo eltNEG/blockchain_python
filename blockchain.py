193a093d680b2f8e020a9e7f6305ecf78485c23b
eltNEG <yorewole@gmail.com>
2018-08-09 09:57:09 +0100

    removed plyvel import

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,5 @@
 from block import Block
-import plyvel
+#import plyvel
 
 
 class Blockchain(Block):
@@ -13,9 +13,9 @@ class Blockchain(Block):
       new_block = self.new_block(_data, prev_block.Hash)
       self.blocks.append(new_block)
 
-      db = plyvel.DB('/tmp/blockchain', create_if_missing=True)
-      db.put(new_block.Hash.encode(), new_block.serialize())
-      db.put(b"l", new_block.Hash.encode())
+      #db = plyvel.DB('/tmp/blockchain', create_if_missing=True)
+      #db.put(new_block.Hash.encode(), new_block.serialize())
+      #db.put(b"l", new_block.Hash.encode())
       
 
     def new_genesis_block(self):
