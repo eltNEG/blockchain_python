2db05dd0098b6f20c01cb5588129a4eb0086420e
eltNEG <yorewole@gmail.com>
2018-08-11 23:21:58 +0100

    added leveldb, blockchainIterator, and re-implement Blockchain

--- blockchain.py
+++ blockchain.py
@@ -1,34 +1,84 @@
 """Blockchain"""
 from block import Block
+from leveldb import LevelDB, LevelDBError
+from sys import exit
+
+dbLocation: str = "blockchain_db"
+
+
+class BlockchainIterator (object):
+    """iterate over blockchain blocks"""
+
+    def __init__(self, current_hash: bytes, db: LevelDB) -> None:
+        """Iterate over blockchain blocks"""
+        self.current_hash: bytes = current_hash
+        self.db: LevelDB = db
+
+    def next(self) -> Block:
+        try:
+            block: Block = Block.deserialise_block(self.db.Get(self.current_hash))
+        except KeyError:
+            print("An error occurred")
+            exit(1)
+        self.current_hash = bytes(block.prev_block_hash.encode())
+        return block
 
 
 class Blockchain(object):
     """Blockchain"""
 
-    def __init__(self, _blocks: list):
+    def __init__(self, tip: bytes, db: LevelDB) -> None:
         """Initialize blockchain which stores a list of blocks"""
-        self.blocks = _blocks
+        self.tip: bytes = tip
+        self.db: LevelDB = db
 
     def add_block(self, _data: str):
         """saves provided data as a block in the blockchain"""
-        prev_block = self.blocks[-1]
-        new_block = Block.new_block(_data, prev_block.hash)
-        self.blocks.append(new_block)
+        try:
+            lasthash: str = self.db.Get(b"tip").decode()
+            new_block: Block = Block.new_block(_data, lasthash)
+        except KeyError:
+            print("An error occur")
+            exit(1)
+        try:
+            tip: bytes = bytes(new_block.hash.encode())
+            self.db.Put(tip, new_block.serialise())
+            self.db.Put(b'tip', tip)
+            self.tip = tip
+        except LevelDBError as e:
+            print(f"An error Occur:\n{e}")
+            exit(1)
+
+    def iterator(self) -> BlockchainIterator:
+        return BlockchainIterator(self.tip, self.db)
 
     @staticmethod
     def new_blockchain():
         """creates a new Blockchain with genesis Block"""
-        return Blockchain([Block.new_genesis_block()])
+        tip: bytes = ""
+        db = LevelDB(dbLocation)
+        try:
+            tip = db.Get(b'tip')
+        except KeyError:
+            print("No existing blockchain found. Creating a new one...")
+            genesis = Block.new_genesis_block()
+            tip = bytes(genesis.hash.encode())
+            try:
+                db.Put(bytes(genesis.hash.encode()), genesis.serialise())
+                db.Put(b'tip', bytes(genesis.hash.encode()), sync=True)
+            except TypeError:
+                print("Type error")
+                exit(1)
+
+        return Blockchain(tip, db)
 
     def __str__(self):
-        return str(self.blocks)
+        return f"Last Hash: {self.tip.decode()}"
 
 
 def main():
     """Tests"""
     bc = (Blockchain.new_blockchain())
-    for block in bc.blocks:
-        print(block)
 
 
 if __name__ == '__main__':
